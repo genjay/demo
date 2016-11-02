1ec3284bc1a918aa405231a46153d5340319869a
ricky <genjay@gmail.com>
2016-11-02 11:56:33 +0800

    add users

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102023752) do
+ActiveRecord::Schema.define(version: 20161102035512) do
 
   create_table "books", force: :cascade do |t|
     t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "sex",        limit: 255
+    t.string   "birthday",   limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
